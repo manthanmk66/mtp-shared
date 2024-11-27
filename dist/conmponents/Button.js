3120abf798e0179a14ee508ac0a1cdc0193202a3
manthanmk66 <manthankaslemk@gmail.com>
2024-11-27 15:46:12 +0530

    build

--- dist/conmponents/Button.js
+++ dist/conmponents/Button.js
@@ -1,5 +1,5 @@
-import { Button as RNButton, makeStyles } from '@rneui/themed';
-import React from 'react';
+import { Button as RNButton, makeStyles } from "@rneui/themed";
+import React from "react";
 const Button = ({ title, style, backgroundColor, ...otherProps }) => {
     const styles = useStyles({ backgroundColor, ...otherProps }); // Pass backgroundColor to useStyles
     return (<RNButton buttonStyle={[styles.buttonStyle, style]} // Apply combined styles
@@ -9,9 +9,9 @@ const Button = ({ title, style, backgroundColor, ...otherProps }) => {
 export { Button };
 const useStyles = makeStyles((theme, props) => ({
     buttonStyle: {
-        borderRadius: 10,
+        borderRadius: 0,
         height: 40,
-        borderWidth: props.type === 'clear' ? 0 : 1,
+        borderWidth: props.type === "clear" ? 0 : 1,
         borderColor: props?.borderColor || theme.colors.primary,
         backgroundColor: props.backgroundColor || theme.colors.primary, // Use backgroundColor from props or theme
     },
